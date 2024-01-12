cbe50ce1216083dc0bc1716022cb12c7f431a434
afisman <afisman@gmail.com>
2024-01-12 16:39:32 +0100

    feat: create store, first api call succesful

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom'
-
+import { Provider } from "react-redux";
 
 import App from './App';
 import 'antd/dist/reset.css';
+import store from "./app/store";
 
 
 const container = document.getElementById('root');
@@ -12,6 +13,8 @@ const root = ReactDOM.createRoot(container);
 
 root.render(
     <BrowserRouter>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </BrowserRouter>
 )
